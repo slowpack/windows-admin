6ff73e3833a9454321015553404b2ec4344e98cd
wp <wp@gmail.com>
2023-06-16 17:32:38 +0800

    妥协

--- src/menu/modules/chatAi.ts
+++ src/menu/modules/chatAi.ts
@@ -9,7 +9,38 @@
 const menus: Menu.recordRaw = {
   title: 'some',
   icon: 'icons8-chatgpt',
-  // children,
+  children: [
+    {
+      title: '老北鼻',
+      icon: 'icons8-goodnotes',
+      windowName: 'gptMirror',
+      params: { url: 'https://url-3.ai-node.com/' },
+    },
+    {
+      title: 'AI天蝴',
+      icon: 'sidebar-default',
+      windowName: 'gptMirror1',
+      params: { url: 'http://chat2.aiyunos.top/' },
+    },
+    {
+      title: 'WoChat',
+      icon: 'sidebar-default',
+      windowName: 'gptMirror2',
+      params: { url: 'https://chat1.wobcw.com/' },
+    },
+    {
+      title: '开放猫',
+      icon: 'sidebar-default',
+      windowName: 'gptMirror3',
+      params: { url: 'https://openmao.panchuang.net/#/' },
+    },
+    {
+      title: 'liao.news',
+      icon: 'sidebar-default',
+      windowName: 'gptMirror4',
+      params: { url: 'https://liao.news/zh' },
+    },
+  ],
 }
 
 export default menus
